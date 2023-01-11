f277e028bd64e4411374fb81bb911ae3927d2089
Jeanne Caron-Guyon <jeanne.caronguyon@gmail.com>
2023-01-11 17:28:36 +0100

    Add files via upload

---
+++ Rezk/fmriprep.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Submission script for Lemaitre3
+#SBATCH --job-name=fmriprep_trial
+#SBATCH --time=42:00:00 # hh:mm:ss
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=9
+#SBATCH --mem-per-cpu=10000 # megabytes
+#SBATCH --partition=batch,debug
+#
+#SBATCH --mail-user=name.surname@uclouvain.be
+#SBATCH --mail-type=ALL
+#sbatch --output=fmriprep_job-%j.txt
+#
+#SBATCH --comment=cpp_cluster_hackaton
+
+module purge
+
+#export OMP_NUM_THREADS=9
+#export MKL_NUM_THREADS=9
+
+subjID=$1
+
+singularity run --cleanenv \
+    -B ../inputs/raw:/bids_dir \ # where the inputs are to : temporary bids_dir directory
+    -B ../tmp:/tmp \ # temporary directory in which analysis is ran (useful when crashs happen to retrieve analysis in process)
+    -B ../outputs:/output \ # where the outputs are to : temporary output directory
+    -B ~/freesurfer_license:/freesurfer_license \  # where the freesurfer license is 
+    ~/sing_temp/containers/images/bids/bids-fmriprep--21.0.1.sing \
+    /bids_dir /output \
+    participant --participant-label ${subjID} \
+    --work-dir /tmp \
+    --fs-license-file /freesurfer_license/license.txt \
+    --output-spaces MNI152NLin2009cAsym T1w \
+    --notrack --stop-on-first-crash \
+    --task visMotion # task to be preprocessed 
